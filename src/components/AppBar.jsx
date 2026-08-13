b8a4938ade4bd45fe9216577d8de0b0a9e57aed5
Miika Valkonen <miika.valkonen08@gmail.com>
2026-08-13 12:33:20 +0300

    7. Scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,4 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
+import { View, StyleSheet, ScrollView} from 'react-native';
 import Text from './Text';
 import theme from './Theme';
 import { Link } from 'react-router-native';
@@ -8,22 +7,46 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.appBarBackground,
     height: 80,
-    justifyContent: 'space-around',
-    paddingTop: 25,
+  },
+  scrollView: {
+    height: 80,
+  },
+  scrollContent: {
     flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 80,
+  },
+  link: {
+    marginHorizontal: 45,
+  },
+  linkText: {
+    color: 'white',
+    fontSize: 22,
   },
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>{/* ... */}
-    <Link to="/" underlayColor="transparent">
-      <Text fontWeight="bold" fontSize="subheading" style={{ color: "white", fontSize: 22}}>Repositories</Text>
-    </Link>
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        horizontal
+        style={styles.scrollView}
+        contentContainerStyle={styles.scrollContent}
+      >
+        <Link to="/" underlayColor="transparent" style={styles.link}>
+          <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
+            Repositories
+          </Text>
+        </Link>
 
-    <Link to="/signIn" underlayColor="transparent">
-      <Text fontWeight="bold" fontSize="subheading" style={{ color: "white", fontSize: 22}}>Sign in</Text>
-    </Link>
-  </View>;
+        <Link to="/signIn" underlayColor="transparent" style={styles.link}>
+          <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
+            Sign in
+          </Text>
+        </Link>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBar;
