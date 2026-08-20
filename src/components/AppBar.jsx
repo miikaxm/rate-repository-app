91a5dd64270c1549264bf80b8ae1b046560be2de
Miika Valkonen <miika.valkonen08@gmail.com>
2026-08-20 12:41:15 +0300

    16. Sign out

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,7 +1,23 @@
-import { View, StyleSheet, ScrollView} from 'react-native';
+import { View, StyleSheet, ScrollView, Pressable} from 'react-native';
 import Text from './Text';
 import theme from './Theme';
 import { Link } from 'react-router-native';
+
+// Apollo
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
+import { useApolloClient } from '@apollo/client/react';
+
+// Storage
+import useAuthStorage from '../hooks/useAuthStorage';
+
+const GET_ME = gql`
+  query Query {
+    me {
+      username
+    }
+  }
+`;
 
 const styles = StyleSheet.create({
   container: {
@@ -26,27 +42,68 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        horizontal
-        style={styles.scrollView}
-        contentContainerStyle={styles.scrollContent}
-      >
-        <Link to="/" underlayColor="transparent" style={styles.link}>
-          <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
-            Repositories
-          </Text>
-        </Link>
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const { data } = useQuery(GET_ME, {
+    context: {
+      headers: {
+        Authorization: `Bearer ${authStorage.getAccessToken()}`,
+      },
+    },
+  });
 
-        <Link to="/signIn" underlayColor="transparent" style={styles.link}>
-          <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
-            Sign in
-          </Text>
-        </Link>
-      </ScrollView>
-    </View>
-  );
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+  }
+
+  if (data?.me === null) {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          horizontal
+          style={styles.scrollView}
+          contentContainerStyle={styles.scrollContent}
+        >
+          <Link to="/" underlayColor="transparent" style={styles.link}>
+            <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
+              Repositories
+            </Text>
+          </Link>
+
+          <Link to="/signIn" underlayColor="transparent" style={styles.link}>
+            <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
+              Sign in
+            </Text>
+          </Link>
+        </ScrollView>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          horizontal
+          style={styles.scrollView}
+          contentContainerStyle={styles.scrollContent}
+        >
+          <Link to="/" underlayColor="transparent" style={styles.link}>
+            <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
+              Repositories
+            </Text>
+          </Link>
+
+          <Link to="/signIn" underlayColor="transparent" style={styles.link}>
+            <Pressable onPress={signOut}>
+              <Text fontWeight="bold" fontSize="subheading" style={styles.linkText}>
+                Sign out
+              </Text>
+            </Pressable>
+          </Link>
+        </ScrollView>
+      </View>
+    );
+  }
 };
 
 export default AppBar;
