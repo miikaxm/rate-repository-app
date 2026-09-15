e1c6ee68dd66d01bee6d17508bece5dd38eba10b
Miika Valkonen <miika.valkonen08@gmail.com>
2026-09-15 09:07:13 +0300

    last fix

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
   }
 });
 
+// add gap between items
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const OrderSelector = ({ selected, setSelected }) => {
