8a3f925e2bf052f17ffc592c17eb38b25ebe4995
Miika Valkonen <miika.valkonen08@gmail.com>
2026-09-15 10:12:19 +0300

    fix: resolve merge conflicts in environment and component files

--- App.js
+++ App.js
@@ -2,11 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-<<<<<<< HEAD
 import { Provider as PaperProvider } from 'react-native-paper';
-=======
-import { PaperProvider } from 'react-native-paper';
->>>>>>> 3d4a9f357973439a6b7c7f6a83ffe0083242aa47
 
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
@@ -19,15 +15,9 @@ const apolloClient = createApolloClient(authStorage);
 const App = () => {
   return (
     <SafeAreaProvider>
-<<<<<<< HEAD
       <PaperProvider>
         <StatusBar style="light" />
 
-=======
-      <StatusBar style="light" />
-
-      <PaperProvider>
->>>>>>> 3d4a9f357973439a6b7c7f6a83ffe0083242aa47
         <NativeRouter
           future={{
             v7_startTransition: true,
